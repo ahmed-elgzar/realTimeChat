4d9db685a61ef5b3b181f782cfe710ec3e8432bb
Ahmed <ahmed2abd0l6ziz@gmail.com>
2020-09-15 22:01:40 +0200

    first commet and designed ui page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,196 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-4">
+            <div class="user-wrapper">
+                <ul class="users">
+                    <li class="user">
+                        <span class="pending">1</span>
+                        <div class="media">
+                            <div class="media-left">
+                                <img src="https://via.placeholder.com/150" alt="" class="media-object"></img>
+                            </div>
+                            <div class="media-body">
+                                <p class="name">Ahmed</p>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+                <ul class="users">
+                    <li class="user">
+                        <span class="pending">1</span>
+                        <div class="media">
+                            <div class="media-left">
+                                <img src="https://via.placeholder.com/150" alt="" class="media-object"></img>
+                            </div>
+                            <div class="media-body">
+                                <p class="name">Ahmed</p>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+                <ul class="users">
+                    <li class="user">
+                        <span class="pending">1</span>
+                        <div class="media">
+                            <div class="media-left">
+                                <img src="https://via.placeholder.com/150" alt="" class="media-object"></img>
+                            </div>
+                            <div class="media-body">
+                                <p class="name">Ahmed</p>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+                <ul class="users">
+                    <li class="user">
+                        <span class="pending">1</span>
+                        <div class="media">
+                            <div class="media-left">
+                                <img src="https://via.placeholder.com/150" alt="" class="media-object"></img>
+                            </div>
+                            <div class="media-body">
+                                <p class="name">Ahmed</p>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+                <ul class="users">
+                    <li class="user">
+                        <span class="pending">1</span>
+                        <div class="media">
+                            <div class="media-left">
+                                <img src="https://via.placeholder.com/150" alt="" class="media-object"></img>
+                            </div>
+                            <div class="media-body">
+                                <p class="name">Ahmed</p>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+                <ul class="users">
+                    <li class="user">
+                        <span class="pending">1</span>
+                        <div class="media">
+                            <div class="media-left">
+                                <img src="https://via.placeholder.com/150" alt="" class="media-object"></img>
+                            </div>
+                            <div class="media-body">
+                                <p class="name">Ahmed</p>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+                <ul class="users">
+                    <li class="user">
+                        <span class="pending">1</span>
+                        <div class="media">
+                            <div class="media-left">
+                                <img src="https://via.placeholder.com/150" alt="" class="media-object"></img>
+                            </div>
+                            <div class="media-body">
+                                <p class="name">Ahmed</p>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="col-md-8" id="messages">
+            <div class="message-wrapper">
+                <ul class="messages">
+                    <li class="message clearfix">
+                        <div class="sent">
+                            <p>Lorem ipsum dolor sit amet</p>
+                            <p class="date">15 sep, 2020</p>
+                        </div>
+                    </li>
+                    <li class="message clearfix">
+                        <div class="received">
+                            <p>Lorem ipsum dolor sit amet</p>
+                            <p class="date">15 sep, 2020</p>
+                        </div>
+                    </li>
+                    <li class="message clearfix">
+                        <div class="sent">
+                            <p>Lorem ipsum dolor sit amet</p>
+                            <p class="date">15 sep, 2020</p>
+                        </div>
+                    </li>
+                    <li class="message clearfix">
+                        <div class="received">
+                            <p>Lorem ipsum dolor sit amet</p>
+                            <p class="date">15 sep, 2020</p>
+                        </div>
+                    </li>
+                    <li class="message clearfix">
+                        <div class="sent">
+                            <p>Lorem ipsum dolor sit amet</p>
+                            <p class="date">15 sep, 2020</p>
+                        </div>
+                    </li>
+                    <li class="message clearfix">
+                        <div class="received">
+                            <p>Lorem ipsum dolor sit amet</p>
+                            <p class="date">15 sep, 2020</p>
+                        </div>
+                    </li>
+                    <li class="message clearfix">
+                        <div class="sent">
+                            <p>Lorem ipsum dolor sit amet</p>
+                            <p class="date">15 sep, 2020</p>
+                        </div>
+                    </li>
+                    <li class="message clearfix">
+                        <div class="received">
+                            <p>Lorem ipsum dolor sit amet</p>
+                            <p class="date">15 sep, 2020</p>
+                        </div>
+                    </li>
+                    <li class="message clearfix">
+                        <div class="sent">
+                            <p>Lorem ipsum dolor sit amet</p>
+                            <p class="date">15 sep, 2020</p>
+                        </div>
+                    </li>
+                    <li class="message clearfix">
+                        <div class="received">
+                            <p>Lorem ipsum dolor sit amet</p>
+                            <p class="date">15 sep, 2020</p>
+                        </div>
+                    </li>
+                    <li class="message clearfix">
+                        <div class="sent">
+                            <p>Lorem ipsum dolor sit amet</p>
+                            <p class="date">15 sep, 2020</p>
+                        </div>
+                    </li>
+                    <li class="message clearfix">
+                        <div class="received">
+                            <p>Lorem ipsum dolor sit amet</p>
+                            <p class="date">15 sep, 2020</p>
+                        </div>
+                    </li>
+                    <li class="message clearfix">
+                        <div class="sent">
+                            <p>Lorem ipsum dolor sit amet</p>
+                            <p class="date">15 sep, 2020</p>
+                        </div>
+                    </li>
+                    <li class="message clearfix">
+                        <div class="received">
+                            <p>Lorem ipsum dolor sit amet</p>
+                            <p class="date">15 sep, 2020</p>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <div class="input-text">
+                <input type="text" name="message" class="submit">
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,194 @@
+<!DOCTYPE html>
+<html lang="{{ config('app.locale') }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <!-- Scripts -->
+    <script>
+        window.Laravel = {!! json_encode([
+            'csrfToken' => csrf_token(),
+        ]) !!};
+    </script>
+    <style>
+        ul {
+            margin: 0;
+            padding: 0;
+        }
+        li {
+            list-style: none;
+        }
+
+        .user-wrapper, .message-wrapper {
+            border: 1px solid #dddddddd;
+            overflow-y: auto; 
+        }
+        .user-wrapper {
+            height: 600px;
+        }
+        .user{
+            cursor: pointer;
+            padding: 5px 0;
+            position: relative;
+        }
+        .user:hover{
+            background: #eeeeeeee; 
+        }
+        .user:last-child{
+            margin-bottom: 0;
+        }
+        .pending {
+            position: absolute;
+            left: 13px;
+            top: 9px;
+            background: #b600ff;
+            margin: 0;
+            border-radius: 50%;
+            width: 18px;
+            height: 18px;
+            line-height: 18px;
+            padding-left: 5px;
+            color: #ffffff;
+            font-size: 12px;
+        }
+        .media-left {
+            margin: 0 10px;
+        }
+        .media-left img {
+            width: 64px;
+            border-radius: 64px;
+        }
+        .media-body p {
+            margin: 6px 0;
+        }
+         .media-body p {
+            margin: 6px 0;
+        }
+        .message-wrapper {
+            padding: 10px;
+            height: 536px;
+            background: #eeeeee;
+        }
+        .messages .message {
+            margin-bottom: 15px;
+        }
+        .messages .message:last-child {
+            margin-bottom: 0;
+        }
+        .received, .sent {
+            width: 45%;
+            padding: 3px 10px;
+            border-radius: 10px;
+        }
+        .received {
+            background: #ffffff;
+            float: left;
+            text-align: left;
+        }
+        .sent {
+            background: #3bebff;
+            float: right;
+            text-align: right;
+        }
+        .message p {
+            margin: 5px 0;
+        }
+        .date {
+            color: #777777;
+            font-size: 12px;
+        }
+        .active {
+            background: #eeeeee;
+        }
+        input[type=text] {
+            width: 100%;
+            padding: 12px 20px;
+            margin: 15px 0 0 0;
+            display: inline-block;
+            border-radius: 4px;
+            box-sizing: border-box;
+            outline: none;
+            border: 1px solid #cccccc;
+        }
+        input[type=text]:focus {
+            border: 1px solid #aaaaaa;
+        }
+    </style>
+</head>
+<body>
+    <div id="app">
+        <nav class="navbar navbar-default navbar-static-top">
+            <div class="container">
+                <div class="navbar-header">
+
+                    <!-- Collapsed Hamburger -->
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                        <span class="sr-only">Toggle Navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+
+                    <!-- Branding Image -->
+                    <a class="navbar-brand" href="{{ url('/') }}">
+                        {{ config('app.name', 'chat
+                        ') }}
+                    </a>
+                </div>
+
+                <div class="collapse navbar-collapse" id="app-navbar-collapse">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="nav navbar-nav">
+                        &nbsp;
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="nav navbar-nav navbar-right">
+                        <!-- Authentication Links -->
+                        @if (Auth::guest())
+                            <li><a href="{{ route('login') }}">Login</a></li>
+                            <li><a href="{{ route('register') }}">Register</a></li>
+                        @else
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+
+                                <ul class="dropdown-menu" role="menu">
+                                    <li>
+                                        <a href="{{ route('logout') }}"
+                                            onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                            Logout
+                                        </a>
+
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                            {{ csrf_field() }}
+                                        </form>
+                                    </li>
+                                </ul>
+                            </li>
+                        @endif
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        @yield('content')
+    </div>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}"></script>
+
+    @yield('script')
+</body>
+</html>
